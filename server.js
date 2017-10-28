a896484594e5febec8fbe331ce9611650080b22b
Jarett Engdahl <jaretten@gmail.com>
2017-10-28 17:48:57 -0400

    crosses fingers

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser');
-var cheeseBurger = require('request')
+var cheeseBurger = require('request');
+var http = require("http");
+
 
 var port = process.env.PORT || 1337
 
@@ -13,17 +15,51 @@ app.get('/', function(req, res){
   res.send("SERVER UP!")
 })
 
-app.post('/getweather', function(req, res){
+app.post('/getweather', function(req, response){
 
   //http://api.apixu.com/v1/current.json?key=488d3421779b4d6ebd6210605172810&q=33523
 
-  cheeseBurger.get("http://api.apixu.com/v1/current.json?key=488d3421779b4d6ebd6210605172810&q=33523")
-  .on('response', function(response) {
-    console.log(response.statusCode) // 200
-    console.log(response.body) // 'image/png'
-  })
+  // cheeseBurger.get("http://api.apixu.com/v1/current.json?key=488d3421779b4d6ebd6210605172810&q=33523")
+  // .on('response', function(response) {
+  //   console.log(response.statusCode) // 200
+  //   console.log(response.location) // 'image/png'
+  //
+  //   res.send(response.location)
+  // })
 
-  res.send("got the request!")
+
+var options = {
+  "method": "GET",
+  "hostname": "api.apixu.com",
+  "port": null,
+  "path": `/v1/current.json?key=488d3421779b4d6ebd6210605172810&q=${req.body.text}`,
+  "headers": {
+    "cache-control": "no-cache",
+  }
+};
+
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    // response.send(body)
+    // console.log(body.toString());
+    response.send(`It is currently ${body.current.temp_f} degrees in ${body.location.name}`)
+
+
+
+  });
+});
+
+req.end();
+
+
+
 })
 
 app.listen(port, function(){
