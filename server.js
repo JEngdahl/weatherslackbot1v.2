d8211c411584b9bd59d47fd5df65e552d85ecdf2
Jarett Engdahl <jaretten@gmail.com>
2017-10-28 17:33:29 -0400

    added request, demo request to API

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser');
+var cheeseBurger = require('request')
+
 var port = process.env.PORT || 1337
 
 
@@ -12,6 +14,15 @@ app.get('/', function(req, res){
 })
 
 app.post('/getweather', function(req, res){
+
+  //http://api.apixu.com/v1/current.json?key=488d3421779b4d6ebd6210605172810&q=33523
+
+  cheeseBurger.get("http://api.apixu.com/v1/current.json?key=488d3421779b4d6ebd6210605172810&q=33523")
+  .on('response', function(response) {
+    console.log(response.statusCode) // 200
+    console.log(response) // 'image/png'
+  })
+
   res.send("got the request!")
 })
 
